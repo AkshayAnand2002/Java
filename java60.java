21dbb59c6ec4b6abc463a8f140103947ba23d05b
AkshayAnand2002 <93818352+AkshayAnand2002@users.noreply.github.com>
2022-08-03 14:12:17 +0530

    Create java60.java

---
+++ java60.java
@@ -0,0 +1,37 @@
+class mymainemployee{
+    private int id;
+    private String name;
+    // public mymainemployee(){
+    //     id=45;
+    //     name="AKSHAY ANAND";
+    // }
+    public mymainemployee(String myname,int myid){
+        id=myid;
+        name=myname;
+    }
+    public String getname(){
+        return name;
+    }
+    public void setname(String n){
+        this.name=n;
+    }
+    public void setid(int i){
+        this.id=i;
+    }
+    public int getid(){
+        return id;
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	    mymainemployee akshay = new mymainemployee("Akshay Anand",12);
+	    //akshay.setname("Akshay Anand");
+	    //System.out.println(akshay.getname());
+	    //ABOVE IS THE CONVENTIONAL WAY OF GETTING AND SETTING
+	    //NOW WE WILL USE CONSTRUCTOR.
+	    System.out.println(akshay.getid());
+	    System.out.println(akshay.getname());
+}
+}
+  
