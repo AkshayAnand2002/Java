fb61db6188d3029265ec0708ea91def4dd8b55ba
AkshayAnand2002 <93818352+AkshayAnand2002@users.noreply.github.com>
2022-08-17 08:19:06 +0530

    Update STAR_PATTERN_1.java

--- STAR_PATTERN_1.java
+++ STAR_PATTERN_1.java
@@ -1,11 +1,39 @@
-public class java24 {
-    public static void main(String[] args) {
-        int n=4;
-        for(int i=n;i>0;i--){
-            for(int j=0;j<i;j++){
-                System.out.print("*");
-            }
-            System.out.print("\n");
-        }
-    }
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
 }
+/*
+OUTPUT-
+*
+**
+***
+****
+*/
+-----------------------------------------------------------------------------
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++){
+		    for(int j=4;j>=i;j--){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
+/*
+OUTPUT-
+****
+***
+**
+*
+*/
