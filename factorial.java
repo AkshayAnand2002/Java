19eb01b717ca623adef906e9689d9233c3de106b
AkshayAnand2002 <93818352+AkshayAnand2002@users.noreply.github.com>
2022-09-09 06:56:12 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,12 @@
+public class Program3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int n=5;
+			int factorial=1;
+			for(int i=5;i>=1;i--) {
+				factorial=factorial*i;
+			}
+			System.out.println(factorial);
+	}
+
+}
