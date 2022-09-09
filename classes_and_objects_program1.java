c15f2295465f8e5b3e56bb67b6a40dfe3b729f13
AkshayAnand2002 <93818352+AkshayAnand2002@users.noreply.github.com>
2022-09-09 07:04:40 +0530

    Create classes_and_objects_program1.java

---
+++ classes_and_objects_program1.java
@@ -0,0 +1,21 @@
+public class program1 {
+		String studentname;
+		int enrollno;
+		String collegename;
+		long mobileno;
+		String city;
+	public static void main(String[] args) {
+		program1 p = new program1();
+		p.studentname="Anil Gupta";
+		p.enrollno=1;
+		p.collegename="LNMIIT";
+		p.mobileno= 9991230987l;
+		p.city="Delhi";
+		System.out.println(p.studentname);
+		System.out.println(p.enrollno);
+		System.out.println(p.collegename);
+		System.out.println(p.mobileno);
+		System.out.println(p.city);
+	}
+
+}
